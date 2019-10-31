a1febb0584841e3c72c282c8189cbdb2f865434a
Gease-RS <geasebrasil@gmail.com>
2019-10-31 11:12:17 -0300

    Funcao Like UnLike e Delete Scream

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -2,7 +2,9 @@ import {
     SET_USER, 
     SET_AUTHENTICATED, 
     SET_UNAUTHENTICATED,
-    LOADING_USER 
+    LOADING_USER, 
+    LIKE_SCREAM,
+    UNLIKE_SCREAM
 } from '../types';
 
 const initialState = {
@@ -28,12 +30,29 @@ export default function(state = initialState, action){
                 loading: false,
                 ...action.payload
             };
-            default: 
-                return state;
         case LOADING_USER:
             return {
                 ...state,
                 loading: true
             };
+        case LIKE_SCREAM:
+            return {
+                ...state,
+                likes: [
+                    ...state.like,
+                    {
+                        userHandle: state.credentials.handle,
+                        screamId: action.payload.screamId
+                    }
+                ]
+            };
+        case UNLIKE_SCREAM:
+            return {
+                ...state,
+                likes: state.likes.filter(
+                    (like) => like.screamId !== action.payload.screamId
+                )
+            };
+            default: return state;
     }
 }
